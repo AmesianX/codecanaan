8ebf050d7944d84e8771ba6e3f5a124718aa5e25
Spooky <smlsun@gmail.com>
2013-01-12 22:27:46 +0800

    新增 CouponTemplateTests unit test

--- test/unit/codecanaan/CouponTemplateTests.groovy
+++ test/unit/codecanaan/CouponTemplateTests.groovy
@@ -1,17 +1,39 @@
 package codecanaan
 
+import static org.junit.Assert.*
 
+import java.util.Date;
 
 import grails.test.mixin.*
+import grails.test.mixin.support.*
 import org.junit.*
 
 /**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @TestFor(CouponTemplate)
+@Mock([Course])
 class CouponTemplateTests {
+	def newCourse=null;
+    void setUp() {
+        newCourse= new Course(
+			name: 'tqc-plus-java6',
+			title: 'TQC+ Java 物件導向程式語言',
+			description: 'course-1 desc',
+		).save();
+	
+    }
 
-    void testSomething() {
-//       fail "Implement me"
+
+
+    void testCouponTemplateCreate() {
+		CouponTemplate.create(
+			"AA",
+			"org",
+			30,
+			newCourse
+		)
+		
+		assert 1,CouponTemplate.list().size()
     }
 }
