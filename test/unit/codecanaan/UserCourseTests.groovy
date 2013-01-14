07c935134a7e7f228803709bcb44c9c5831cc7c5
Spooky <smlsun@gmail.com>
2013-01-14 21:29:45 +0800

    add UserCourse unit test

--- test/unit/codecanaan/UserCourseTests.groovy
+++ test/unit/codecanaan/UserCourseTests.groovy
@@ -9,9 +9,29 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(UserCourse)
+@Mock([User,Course])
 class UserCourseTests {
-
-    void testSomething() {
-//       fail "Implement me"
+	def user=null
+	def newCourse =null
+	void setup(){
+		user= new User(username: 'admin', password: 'admin').save();
+		newCourse=new Course(
+			name: 'tqc-plus-java6',
+			title: 'TQC+ Java 物件導向程式語言',
+			description: 'course-1 desc',
+			creator: user
+		).save()
+	}
+	
+    void testSaveUserCourse() {
+		UserCourse.create(user,newCourse,true)
+		
+		assert 1, UserCourse.findByUserAndCourse(user,newCourse).list().size()
     }
+	
+	void testSaveUserCourseWithRegType() {
+		UserCourse.create(user,newCourse,RegType.USER,true)
+		
+		assert RegType.USER, UserCourse.findByUserAndCourse(user,newCourse).list().get(0).regType
+	}
 }
