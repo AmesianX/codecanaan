aef8b580e8b9dd72fb8967e0ba1a3a1c805cc324
Spooky <smlsun@gmail.com>
2013-01-15 12:13:57 +0800

    add ScheduleService unit test

--- test/unit/codecanaan/ScheduleServiceTests.groovy
+++ test/unit/codecanaan/ScheduleServiceTests.groovy
@@ -9,9 +9,26 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ScheduleService)
+@Mock([ScheduleLesson, User, UserSchedule, Schedule, Lesson])
 class ScheduleServiceTests {
+	def newScheduleLesson=null
 
-    void testSomething() {
-//        fail "Implement me"
+	void setUp(){
+		UserSchedule.create(new User(), new Schedule(), true)	
+		newScheduleLesson=new ScheduleLesson(
+			schedule: new Schedule(), 
+			lesson: new Lesson(),
+			begin: new Date(), 
+			end: new Date(), 
+			deadline: new Date())
+	}
+	
+    void testHasUser() {
+		def ss=new ScheduleService()
+		assertTrue ss.hasUser(new Schedule(), new User())
     }
+	void testAnswerAvailable() {
+		def ss=new ScheduleService()
+		assertFalse ss.answerAvailable(newScheduleLesson)
+	}
 }
