1215549546853e65d4c55dfd8d68b65565e46dd9
Spooky <smlsun@gmail.com>
2013-01-16 10:16:24 +0800

    unit test fail

--- test/unit/codecanaan/UserRoleTests.groovy
+++ test/unit/codecanaan/UserRoleTests.groovy
@@ -9,7 +9,7 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(UserRole)
-@Mock([User, Role, UserRole])
+@Mock([User, Role])
 class UserRoleTests {
 	def role1 =null
 	def user1 =null
@@ -18,31 +18,27 @@ class UserRoleTests {
 		User.metaClass.encodePassword = {}
 		
 		role1 = Role.findOrSaveByAuthority('ROLE_USER')
-		user1 = new User(username: 'student', password: 'student', enabled: true).save(failOnError: true, flush: true)
-		user2 = new User(username: 'teacher', password: 'teacher', enabled: true).save(failOnError: true, flush: true)
-		
-		new UserRole(
-			user: user1,
-			role: role1
-		).save()
+		user1 = new User(username: 'student', password: 'student', enabled: true).save(failOnError: true, flush: true)		
 	}
     void "test call UserRole create" () {
-		UserRole.create(user2,role1,true)
+		UserRole.create(user1,role1,true)
 		
 		assert 2, UserRole.list().size() 
     }
 	
 	void "test call UserRole remove" () {
+		
+		UserRole.create(user1,role1,true)
+		
 		assertNotNull UserRole.findByRole(role1)
 		
-		assert UserRole.list().size() == 1
+		UserRole.remove(user1, role1, true)
 		
-		assertTrue UserRole.remove(user1, role1, true)
-		
-		assert UserRole.list().size() == 0
+		assertNull UserRole.findByRoleAndUser(role1, user1)
 	}
 	
 	void "test ccall UserRole hashCode" () {
+		UserRole.create(user1,role1,true)
 		assertNotNull UserRole.findByRole(role1).hashCode()
 		
 	}
